7bb364bb2ae7deffb49981653a633d9d45facd40
Mdnsrf <40799261+Mdnsrf@users.noreply.github.com>
2024-01-21 11:01:58 +0530

    Update forgetpasswordcompany.aspx.cs

--- forgetpasswordcompany.aspx.cs
+++ forgetpasswordcompany.aspx.cs
@@ -38,7 +38,7 @@ public partial class forgetpasswordcompany : System.Web.UI.Page
 
             mobile = TextBox2.Text.ToString();
             message = String.Concat("PASSWORD FOR YOUR TALENT HUB ACCOUNT IS:", TextBox1.Text.ToString()," DO NOT SHARE THIS WITH ANYONE.");
-            string x1 = apicall("http://smscp.paceinfonet.com/api/sendmsg.php?user=dhavalpatel&pass=dhaval$R$patel&sender=NAAVID&phone=" + mobile + " &text=" + message + " &priority=ndnd&stype=normal");
+            string x1 = apicall("http://smscp.paceinfonet.com/api/&sender=NAAVID&phone=" + mobile + " &text=" + message + " &priority=ndnd&stype=normal");
             Label1.Text = "Password is sent to your registered mobile number.";
 
 
